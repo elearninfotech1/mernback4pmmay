b380196b2aeeb0d71ec19257f0c5daeeb95d92cc
elearninfotech1 <91933957+elearninfotech1@users.noreply.github.com>
2025-05-15 15:32:21 +0530

    fff

--- app.js
+++ app.js
@@ -2,22 +2,16 @@ let express = require("express");
 require("./dbconfig/dbconfig");
 let cors = require("cors");
 const studentRouting = require("./routing/studentRouting");
-const signupRouting = require("./routing/signupRouting");
-const treatmentRouting = require("./routing/treatmentRouting");
-const offerRouting = require("./routing/offerRouting");
-const appointmentRouting = require("./routing/appointmentRouting");
-const contactusRouting = require("./routing/contactusRouting");
-const doctorRouting = require("./routing/doctorRouting");
+
 let app = express();
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cors());
 app.use("/", studentRouting);
-app.use("/", signupRouting);
-app.use("/", treatmentRouting);
-app.use("/", offerRouting);
-app.use("/", appointmentRouting);
-app.use("/", contactusRouting);
-app.use("/", doctorRouting);
 
-app.listen(4000, () => console.log("Server is started on 4000 port"));
+app.get("/", (req, res) => {
+  res.send("✅ API is live and working!");
+});
+
+app.listen(PORT, () => console.log(`Server is started on port ${PORT}`));
