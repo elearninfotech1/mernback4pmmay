200aff728a90145316b5b7a505f00332cb611336
elearninfotech1 <91933957+elearninfotech1@users.noreply.github.com>
2025-05-15 15:35:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let express = require("express");
 require("./dbconfig/dbconfig");
 let cors = require("cors");
 const studentRouting = require("./routing/studentRouting");
+const treatmentRouting = require("./routing/treatmentRouting");
 
 let app = express();
 const PORT = process.env.PORT || 4000;
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cors());
 app.use("/", studentRouting);
+app.use("/", treatmentRouting);
 
 app.get("/", (req, res) => {
   res.send("✅ API is live and working!");
